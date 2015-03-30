eef943c2c39722777999c642ca2569b6a3320eb1
nomper <kenya.5528@gmail.com>
2015-03-30 23:53:37 +0900

    15 03 30
    ビルドできない。
    stm32f4xe.hが見つからない。

---
+++ Program/build/main.d
@@ -0,0 +1,7 @@
+.\build\main.o: main.cpp
+.\build\main.o: mbed\mbed.h
+.\build\main.o: mbed\platform.h
+.\build\main.o: mbed/TARGET_NUCLEO_F411RE/TARGET_STM/TARGET_NUCLEO_F411RE/device.h
+.\build\main.o: mbed/TARGET_NUCLEO_F411RE/TARGET_STM/TARGET_NUCLEO_F411RE/objects.h
+.\build\main.o: mbed/TARGET_NUCLEO_F411RE/cmsis.h
+.\build\main.o: mbed/TARGET_NUCLEO_F411RE/stm32f4xx.h
